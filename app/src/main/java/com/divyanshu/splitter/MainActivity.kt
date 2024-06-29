5c872d2d00fc1c8642d51a07d7c7b333427d04a4
DIVYANSHU TALAN <62091105+Dtalan@users.noreply.github.com>
2024-06-30 02:02:07 +0530

    URL input from user
    basic changes to get URL or some input from user and check if app can access that location and do something about it.

--- app/src/main/java/com/divyanshu/splitter/MainActivity.kt
+++ app/src/main/java/com/divyanshu/splitter/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.divyanshu.splitter
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.divyanshu.splitter.driveInteractions.DriveInteraction
 import com.divyanshu.splitter.ui.theme.SplitterTheme
 
 class MainActivity : ComponentActivity() {
+
+    var driveInteraction : DriveInteraction = DriveInteraction()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             SplitterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,10 +47,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var mUrl by remember { mutableStateOf("Hello") }
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TextField(
+            value = mUrl,
+            onValueChange = {mUrl = it},
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        Button(onClick = {
+            val driveInteraction = DriveInteraction()
+            driveInteraction.accessDrive(mUrl)
+        }) {
+            Text("Check Access")
+        }
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/divyanshu/splitter/driveInteractions/DriveInteraction.kt
@@ -0,0 +1,11 @@
+package com.divyanshu.splitter.driveInteractions
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+class DriveInteraction {
+
+    fun accessDrive(mUrl : String){
+    }
+}
