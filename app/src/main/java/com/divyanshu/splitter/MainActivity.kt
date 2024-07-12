19dc9f4bbbda0001e001dda4fa185dc388d7a62d
Adarsh Srivastava <imadarshsri@gmail.com>
2024-07-12 11:35:24 +0530

    Removes dummy code (#13)

--- app/src/main/java/com/divyanshu/splitter/MainActivity.kt
+++ app/src/main/java/com/divyanshu/splitter/MainActivity.kt
@@ -1,35 +1,25 @@
 package com.divyanshu.splitter
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.divyanshu.splitter.driveInteractions.DriveInteraction
 import com.divyanshu.splitter.ui.theme.SplitterTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContent {
             SplitterTheme {
                 HomeScreen()
@@ -40,7 +30,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    var mUrl by remember { mutableStateOf("Hello") }
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
             modifier = Modifier
@@ -49,17 +38,14 @@ fun HomeScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TextField(
-                value = mUrl,
-                onValueChange = { mUrl = it },
-                modifier = Modifier.padding(bottom = 10.dp)
+            Text(
+                text = "Welcome to Splitter!!",
+                modifier = Modifier.padding(10.dp)
             )
-            Button(onClick = {
-                val driveInteraction = DriveInteraction()
-                driveInteraction.accessDrive(mUrl)
-            }) {
-                Text("Check Access")
-            }
+            Text(
+                text = "Coming soon...",
+                modifier = Modifier.padding(10.dp)
+            )
         }
     }
 }
