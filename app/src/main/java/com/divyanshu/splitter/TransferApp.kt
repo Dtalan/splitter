8955348f1c8a7baa5387205abeac98094afade81
Adarsh Srivastava <imadarshsri@gmail.com>
2024-07-28 12:18:34 +0530

    Integrates Firebase in App (#20)

--- app/src/main/java/com/divyanshu/splitter/TransferApp.kt
+++ app/src/main/java/com/divyanshu/splitter/TransferApp.kt
@@ -1,12 +1,13 @@
 package com.divyanshu.splitter
 
 import android.app.Application
-import android.content.Context
 import com.divyanshu.splitter.common.utils.ContextProvider
+import com.google.firebase.FirebaseApp
 
 class TransferApp : Application() {
     override fun onCreate() {
         super.onCreate()
         ContextProvider.setContext(this)
+        FirebaseApp.initializeApp(this)
     }
 }
